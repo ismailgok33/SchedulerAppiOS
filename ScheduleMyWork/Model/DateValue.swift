592af7865e6e31c217fb6c7cd489780dcfdbdf59
İsmail <ismailgok33@gmail.com>
2021-11-21 19:11:01 +0300

    Started to desing the calendar View

---
+++ ScheduleMyWork/Model/DateValue.swift
@@ -0,0 +1,20 @@
+//
+//  DateValue.swift
+//  ScheduleMyWork
+//
+//  Created by İsmail on 21.11.2021.
+//
+
+import SwiftUI
+
+struct DateValue: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DateValue_Previews: PreviewProvider {
+    static var previews: some View {
+        DateValue()
+    }
+}
---
+++ ScheduleMyWork/View/Calendar/CustomDatePicker.swift
@@ -0,0 +1,20 @@
+//
+//  CustomDatePicker.swift
+//  ScheduleMyWork
+//
+//  Created by İsmail on 21.11.2021.
+//
+
+import SwiftUI
+
+struct CustomDatePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomDatePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomDatePicker()
+    }
+}
